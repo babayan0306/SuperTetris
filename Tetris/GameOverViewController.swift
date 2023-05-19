411071d08ac114e01f0235807c22f70410dae468
babayan0306 <125582118+babayan0306@users.noreply.github.com>
2023-05-19 22:19:27 +0400

    final

---
+++ Tetris/GameOverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameOverViewController.swift
+//  Tetris
+//
+//  Created by Yurka Babayan on 01.04.23.
+//  Copyright © 2023 SchwiftyUI. All rights reserved.
+//
+
+import UIKit
+
+class GameOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
